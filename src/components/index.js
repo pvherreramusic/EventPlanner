564398cb9fb73734a41c4e5bbf95dc8f9713d1f9
Patrick-Vincent Herrera <patrick-vincentherrera@Patrick-Vincents-MacBook-Air.local>
2020-09-05 22:44:27 -0700

    AHHHHH

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,42 @@
-export { default as HomePage } from "./homepage";
-export { default as Nav } from "./nav";
-export { default as Login } from "./login";
-export { default as FormForNewEvent } from "./neweventformpage";
-export { default as Example } from "./userevent";
-export { default as GroupPage } from "./groupeventpage";
-export {default as CommentForm} from "./commentbox";
-export {default as Comments} from "./mappcoments";
-export {default as CreateGroup} from "./groupsforms"
-export {default as SelectGroups} from "./selectgroup";
-export {default as MainPage} from "./mainpage";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import { HomePage, Nav, FormForNewEvent, CreateGroup, MainPage, GroupPage} from ".";
+
+const App = () => {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const bearer = {
+        headers: { Authorization: `Bearer ${token}` },
+      };
+
+      axios.post("api/users/login/token", {}, bearer).then((res) => {
+        const userData = res.data;
+        return setUser(userData);
+      });
+    }
+  }, []);
+
+  return (
+    <Router>
+      <Nav user={user} setUser={setUser} />
+      
+        <Route path="/" exact={true} component={HomePage} />
+        <Route path="/groupevent" exact render={() => <MainPage userid={user.id} />}/>
+        <Route path= "/neweventform" exact render={() => <FormForNewEvent userid={user.id} />  }/>
+        <Route path= "/createnewgroup" exact component={CreateGroup}/>
+
+
+         
+
+
+      
+    </Router>
+  );
+};
+ReactDOM.render(<App />, document.getElementById("app"));
